34230b27d869453977439008268b9bd5e220dc1e
Dorian Scholz <112284719+Dorian-Scholz@users.noreply.github.com>
2024-12-08 22:42:26 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,48 @@
-function hasCycle(graph) {
-    return false;
+function hasCycle(graph) 
+{
+    
+
+    const visited = new Set();
+    const recursionStack = new Set();
+
+    function dfs(node) 
+    {
+        if (recursionStack.has(node)) 
+        {
+            
+            return true;
+        }
+
+        if (visited.has(node))
+        {
+            return false;
+        }
+
+        visited.add(node);
+        recursionStack.add(node);
+
+        for (const neighbor of graph[node] || [])
+            {
+            if (dfs(neighbor)) 
+            {
+                return true;
+            }
+        }
+
+        recursionStack.delete(node);
+        return false;
+    }
+
+     for (const node in graph) 
+    {
+        if (!visited.has(node)) 
+        {
+            if (dfs(node))
+            {
+                return true;
+            }
+        }
+    }
+
+    return false; 
 }
